ed805efc286f8b3f525c9fb3365f51bc40957403
Braxton Shoop <braxton.shoop@gmail.com>
2020-11-26 12:38:57 -0600

    Exercise 4 completed

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -5,7 +5,13 @@ FROM Grade gr
 */
 
 --Exercise 2
+/*
 SELECT e.Name
 FROM PoemEmotion pe
 LEFT OUTER JOIN Emotion e ON e.Id = pe.Id
 WHERE e.Name != 'NULL'
+*/
+
+--Exercise 3
+SELECT COUNT(DISTINCT p.Title)
+FROM  Poem p
